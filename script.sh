79a6c7858581222f711119c666fc53877d661ae1
zerosecxz <155688584+zerosecxz@users.noreply.github.com>
2024-07-03 23:29:42 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
-# Color variables
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-NC='\033[0m'
+# Update and upgrade system
+echo "Updating and upgrading system..."
+sudo apt update -y && sudo apt upgrade -y
+echo "System updated and upgraded."
 
-echo -e "\n${BLUE}Installing aligned_layer and setting up environment...${NC}\n"
+# Install curl
+echo "Installing curl..."
+sudo apt-get install curl -y
+echo "Curl installed."
+
+# Download and install ALignedProof
+echo "Downloading and installing ALignedProof..."
 curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/install_aligned.sh | bash
-source /home/gitpod/.bashrc  # Ensure .bashrc is sourced to update PATH
+source /root/.bashrc
+echo "ALignedProof installed."
 
-echo -e "\n${BLUE}Getting proof test files...${NC}\n"
+# Download an example SP1 proof file with its ELF file
+echo "Downloading SP1 proof file and ELF file..."
 curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/get_proof_test_files.sh | bash
+echo "SP1 proof file and ELF file downloaded."
 
-echo -e "\n${BLUE}Removing existing verification data (if any)...${NC}\n"
-rm -rf ~/.aligned/aligned_verification_data/
-
-export PATH="$PATH:/home/gitpod/.aligned/bin"
-
-echo -e "\n${BLUE}Submitting proof to aligned_layer...${NC}\n"
+# Sending proof
+echo "Sending proof..."
+rm -rf ~/aligned_verification_data/
 aligned submit \
 --proving_system SP1 \
 --proof ~/.aligned/test_files/sp1_fibonacci.proof \
---vm_program ~/.aligned/test_files/sp1_fibonacci.elf \
---aligned_verification_data_path ~/.aligned/aligned_verification_data \
+--vm_program ~/.aligned/test_files/sp1_fibonacci-elf \
+--aligned_verification_data_path ~/aligned_verification_data \
 --conn wss://batcher.alignedlayer.com
+echo "Proof sent."
 
-sleep 60
-
-echo -e "\n${BLUE}Verifying proof onchain...${NC}\n"
+# Verify proof on-chain
+echo "Verifying proof on-chain..."
 aligned verify-proof-onchain \
---aligned-verification-data ~/.aligned/aligned_verification_data/*.json \
+--aligned-verification-data ~/aligned_verification_data/*.json \
 --rpc https://ethereum-holesky-rpc.publicnode.com \
 --chain holesky
+echo "Proof verification on-chain complete."
 
-echo -e "\n${GREEN}Script execution completed.${NC}\n"
+echo "Script execution completed. Check the logs for details."
